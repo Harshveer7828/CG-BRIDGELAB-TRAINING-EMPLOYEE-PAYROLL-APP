23f7ac1652a8c69b2aa92425a75a759e111cc7dd
harshveer7828 <harshveer7828@gmail.com>
2025-02-24 19:37:06 +0530

    [Harsh veer singh] Add.Add Homepage in the app✨

---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeepayrollapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping
+    public String homepage(){
+        return "WELCOME TO THE EMPLOYEE PAYROLL APP";
+    }
+
+
+}
