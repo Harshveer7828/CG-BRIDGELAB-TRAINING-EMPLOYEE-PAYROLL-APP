6272c7b7b75b80382658c23167612f6125bd85e3
harshveer7828 <harshveer7828@gmail.com>
2025-02-25 15:45:08 +0530

    [Harsh veer singh] Add. Integerate lambok for using its features and remove getter setters from the employee✨

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
@@ -2,10 +2,12 @@ package com.bridgelabz.employeepayrollapp.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 @Entity
 @Table(name = "employees")
-public class Employee {
+@Data
+public class  Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,35 +27,5 @@ public class Employee {
         this.salary = salary;
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
 }
